cbc53a33957325957906c3f1c8d1cd220ccbf979
ntelinasia <ana.ntelif@gmail.com>
2024-09-06 13:45:45 +0300

    Initial commit

---
+++ src/main/java/com/antelif/fileaggregator/FileAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.antelif.fileaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAggregatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FileAggregatorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/antelif/fileaggregator/FileAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antelif.fileaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileAggregatorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
